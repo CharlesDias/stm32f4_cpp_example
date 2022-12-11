22f4a3e0d18d747291f11f36dc25a7b785250fcd
CharlesDias <charlesdias@tecnofuturo.com.br>
2022-12-11 08:44:15 -0300

    feat: add initial projects

---
+++ src/stm32f4-cpp-project-app/Application/Src/app_main.cpp
@@ -0,0 +1,13 @@
+/*
+ * app_main.cpp
+ *
+ *  Created on: Dec 11, 2022
+ *      Author: charlesdias
+ */
+
+#include "../../Inc/app_main.h"
+
+void app_main(void)
+{
+
+}
---
+++ src/stm32f4-cpp-project-app/Inc/app_main.h
@@ -0,0 +1,13 @@
+/*
+ * app_main.h
+ *
+ *  Created on: Dec 11, 2022
+ *      Author: charlesdias
+ */
+
+#ifndef APP_MAIN_H_
+#define APP_MAIN_H_
+
+void app_main(void);
+
+#endif /* APP_MAIN_H_ */
